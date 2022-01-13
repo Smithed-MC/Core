e49de74eebb5bd1d6a7260dbd02c971f6007bb9d
TheNuclearNexus <thenuclearnexus@gmail.com>
2022-01-13 10:42:01 -0500

    updated versioning

--- plugins/versioning.py
+++ plugins/versioning.py
@@ -3,70 +3,71 @@ from beet.toolchain.context import Context
 
 call = (
     'execute '
-        'if score #smithed.core.major load.status matches {major} '
-        'if score #smithed.core.minor load.status matches {minor} '
-        'if score #smithed.core.patch load.status matches {patch} '
+        'if score {key}.major load.status matches {major} '
+        'if score {key}.minor load.status matches {minor} '
+        'if score {key}.patch load.status matches {patch} '
         'run function {path}'
 )
 
 resolve_text = (
-    'schedule clear smithed:core/impl/{version}/technical/tick\n'
+    'schedule clear {root}/impl/{version}/technical/tick\n'
     'execute '
-        'if score #smithed.core.major load.status matches {major} '
-        'if score #smithed.core.minor load.status matches {minor} '
-        'if score #smithed.core.patch load.status matches {patch} '
-        'run function smithed:core/impl/{version}/technical/load'
+        'if score {key}.major load.status matches {major} '
+        'if score {key}.minor load.status matches {minor} '
+        'if score {key}.patch load.status matches {patch} '
+        'run function {root}/impl/{version}/technical/load'
 )
 
 enumerate_text = (
-    'scoreboard players add #smithed.core.major load.status 0\n'
-    'scoreboard players add #smithed.core.minor load.status 0\n'
-    'scoreboard players add #smithed.core.patch load.status 0\n'
-    'function smithed:core/calls/{version}/technical/enumerate/major\n'
-    'scoreboard players reset #smithed.core.set load.status\n'
+    'scoreboard players add {key}.major load.status 0\n'
+    'scoreboard players add {key}.minor load.status 0\n'
+    'scoreboard players add {key}.patch load.status 0\n'
+    'function {root}/calls/{version}/technical/enumerate/major\n'
+    'scoreboard players reset {key}.set load.status\n'
 )
 
 major_text = (
     'execute '
-        'if score #smithed.core.major load.status matches ..{major} '
-        'unless score #smithed.core.major load.status matches {major} '
-        'run function smithed:core/calls/{version}/technical/enumerate/set_version\n'
+        'if score {key}.major load.status matches ..{major} '
+        'unless score {key}.major load.status matches {major} '
+        'run function {root}/calls/{version}/technical/enumerate/set_version\n'
     'execute '
-        'if score #smithed.core.major load.status matches ..{major} '
-        'if score #smithed.core.major load.status matches {major} '
-        'unless score #smithed.core.set load.status matches 1 '
-        'run function smithed:core/calls/{version}/technical/enumerate/minor'
+        'if score {key}.major load.status matches ..{major} '
+        'if score {key}.major load.status matches {major} '
+        'unless score {key}.set load.status matches 1 '
+        'run function {root}/calls/{version}/technical/enumerate/minor'
 )
 
 minor_text = (
     'execute '
-        'if score #smithed.core.minor load.status matches ..{minor} '
-        'unless score #smithed.core.minor load.status matches {minor} '
-        'run function smithed:core/calls/{version}/technical/enumerate/set_version\n'
+        'if score {key}.minor load.status matches ..{minor} '
+        'unless score {key}.minor load.status matches {minor} '
+        'run function {root}/calls/{version}/technical/enumerate/set_version\n'
     'execute '
-        'if score #smithed.core.minor load.status matches ..{minor} '
-        'if score #smithed.core.minor load.status matches {minor} '
-        'unless score #smithed.core.set load.status matches 1 '
-        'run function smithed:core/calls/{version}/technical/enumerate/patch'
+        'if score {key}.minor load.status matches ..{minor} '
+        'if score {key}.minor load.status matches {minor} '
+        'unless score {key}.set load.status matches 1 '
+        'run function {root}/calls/{version}/technical/enumerate/patch'
 )
 
 patch_text = (
     'execute '
-        'if score #smithed.core.patch load.status matches ..{patch} '
-        'unless score #smithed.core.patch load.status matches {patch} '
-        'run function smithed:core/calls/{version}/technical/enumerate/set_version'
+        'if score {key}.patch load.status matches ..{patch} '
+        'unless score {key}.patch load.status matches {patch} '
+        'run function {root}/calls/{version}/technical/enumerate/set_version'
 )
 
 set_version = (
-    'scoreboard players set #smithed.core.major load.status {major}\n'
-    'scoreboard players set #smithed.core.minor load.status {minor}\n'
-    'scoreboard players set #smithed.core.patch load.status {patch}\n'
-    'scoreboard players set #smithed.core.set load.status 1'
+    'scoreboard players set {key}.major load.status {major}\n'
+    'scoreboard players set {key}.minor load.status {minor}\n'
+    'scoreboard players set {key}.patch load.status {patch}\n'
+    'scoreboard players set {key}.set load.status 1'
 )
-
 
 def beet_default(ctx: Context):
     version = ctx.template.globals['version'] = f'v{ctx.project_version}'
+    key = ctx.meta['versioning']['key']
+    root = ctx.meta['versioning']['root']
     major, minor, patch = version.replace('v', '').split('.')
 
     yield
@@ -78,42 +79,42 @@ def beet_default(ctx: Context):
     for path in ctx.data.functions.match('impl'):
         first_line = ctx.data.functions[path].text.split('\n')[0]
         if first_line.startswith('#') and '@public' in first_line:
-            generate_call(ctx, path, version)
+            generate_call(ctx, path, version, root, key)
 
-    ctx.data[f'smithed:core/calls/{version}/technical/resolve'] = Function(
-        resolve_text.format(version=version, major=major, minor=minor, patch=patch)
+    ctx.data[f'{root}/calls/{version}/technical/resolve'] = Function(
+        resolve_text.format(root=root, key=key, version=version, major=major, minor=minor, patch=patch)
     )
 
-    ctx.data[f'smithed:core/calls/{version}/technical/enumerate'] = Function(
-        enumerate_text.format(version=version)
+    ctx.data[f'{root}/calls/{version}/technical/enumerate'] = Function(
+        enumerate_text.format(root=root, key=key, version=version)
     )
 
-    ctx.data[f'smithed:core/calls/{version}/technical/enumerate/major'] = Function(
-        major_text.format(version=version, major=major)
+    ctx.data[f'{root}/calls/{version}/technical/enumerate/major'] = Function(
+        major_text.format(root=root, key=key, version=version, major=major)
     )
 
-    ctx.data[f'smithed:core/calls/{version}/technical/enumerate/minor'] = Function(
-        minor_text.format(version=version, minor=minor)
+    ctx.data[f'{root}/calls/{version}/technical/enumerate/minor'] = Function(
+        minor_text.format(root=root, key=key, version=version, minor=minor)
     )
 
-    ctx.data[f'smithed:core/calls/{version}/technical/enumerate/patch'] = Function(
-        patch_text.format(version=version, patch=patch)
+    ctx.data[f'{root}/calls/{version}/technical/enumerate/patch'] = Function(
+        patch_text.format(root=root, key=key, version=version, patch=patch)
     )
 
-    ctx.data[f'smithed:core/calls/{version}/technical/enumerate/set_version'] = Function(
-        set_version.format(version=version, major=major, minor=minor, patch=patch)
+    ctx.data[f'{root}/calls/{version}/technical/enumerate/set_version'] = Function(
+        set_version.format(root=root, key=key, version=version, major=major, minor=minor, patch=patch)
     )
     
-    ctx.data[f'smithed:core/load/enumerate'] = FunctionTag(
-        {"values":["smithed:core/calls/{version}/technical/{tag}".format(version=version, tag='enumerate')]}
+    ctx.data[f'{root}/load/enumerate'] = FunctionTag(
+        {"values":["{root}/calls/{version}/technical/{tag}".format(root=root, version=version, tag='enumerate')]}
     )
-    ctx.data[f'smithed:core/load/resolve'] = FunctionTag(
-        {"values":["smithed:core/calls/{version}/technical/{tag}".format(version=version, tag='resolve')]}
+    ctx.data[f'{root}/load/resolve'] = FunctionTag(
+        {"values":["{root}/calls/{version}/technical/{tag}".format(root=root, version=version, tag='resolve')]}
     )
 
 
-def generate_call(ctx, path: str, version: str):
-    print('  api:', '#smithed:core/pub/' + path.split(version)[1][1:])
+def generate_call(ctx, path: str, version: str, root: str, key: str):
+    print('  api:', f'#{root}/pub/' + path.split(version)[1][1:])
     major, minor, patch = version.replace('v', '').split('.')
 
     tag = {
@@ -122,5 +123,5 @@ def generate_call(ctx, path: str, version: str):
 
     tag['values'].append(path.replace('impl', 'calls'))
 
-    ctx.data['smithed:core/pub/' + path.split(version)[1][1:]] = FunctionTag(tag)
-    ctx.data[path.replace('impl', 'calls')] = Function(call.format(major=major, minor=minor, patch=patch, path=path))
+    ctx.data[f'{root}/pub/' + path.split(version)[1][1:]] = FunctionTag(tag)
+    ctx.data[path.replace('impl', 'calls')] = Function(call.format(major=major, minor=minor, patch=patch, path=path, key=key))
